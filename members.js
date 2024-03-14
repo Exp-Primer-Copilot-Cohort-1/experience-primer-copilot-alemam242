fe7a73cf698e6dbe025384c0f8964be70603d451
Md. Al Emam <76738557+alemam242@users.noreply.github.com>
2024-03-14 07:55:19 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+    return [
+        {name: 'John', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Anna', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Sara', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Paul', skills: ['JavaScript', 'React', 'Node']}
+    ];
+}
